d8dc933024e608c1ed00000aca52cfe8069f721c
Hannahbronwing <hannahsdoc.2015@gmail.com>
2022-05-14 11:41:47 -0400

    commit tree d31118e4762d07e29e1e0a4272a292f608c7d700

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a,b) {
+    return a + b
+}
+function subtract(b,a) {
+    return b - a
+}
+function multiply(a,b) {
+    return a*b
+}
+function divide(a,b) {
+    return a/b
+}
+function increment(a) {
+    return (a+=1)
+}
+function decrement(a) {
+    return a-=1
+}
+function makeInt(string) {
+    return parseInt(string,10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
